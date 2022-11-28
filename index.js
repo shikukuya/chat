627a4034b702dec3c272b7a5757a7afa734bc8e6
shikukuya <shikukuya@outlook.com>
2022-11-28 20:51:44 +0800

    commit

--- index.js
+++ index.js
@@ -155,9 +155,6 @@ function login(user, password) {
     } else {
       username = user;
       readonly = false;
-      var el = document.createElement("div");
-      el.innerText = "登录为：" + username;
-      el.classList.add("info");
       document.querySelector("#messages").append(el);
       document.querySelector("#login").style.display = "none";
     }
